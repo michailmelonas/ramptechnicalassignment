035196a3475c4679a274b7827c3f65b18e1d726a
Michail Melonas <michail@Michails-MacBook-Pro.local>
2020-10-25 07:37:22 +0200

    updated train_model script

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,14 +1,17 @@
+from newusers import NewUsers
 from retention import RetentionCurve
 import pandas as pd
 
+_DAYS = 180
 _PROCESSED_DATA_PATH = '../../data/processed/'
 _REPORTS_FIGURES_PATH = '../../reports/figures/'
+
 
 # Get pre-processed data
 df = pd.read_csv(_PROCESSED_DATA_PATH + 'usage_data.csv')
 
 # Data for fitting retention curve
-weighted_triangle_df = df.tail(180)
+weighted_triangle_df = df.tail(_DAYS)
 weighted_triangle_df = weighted_triangle_df.reset_index(drop=True)
 weighted_triangle_df = weighted_triangle_df.dropna(axis=1, how='all')
 
@@ -20,7 +23,23 @@ rc = RetentionCurve()
 rc.fit(df=weighted_triangle_df)
 
 # Visualize retention curve
-plt = rc.visualize(df=weighted_triangle_df)
-plt.savefig(_REPORTS_FIGURES_PATH + 'retention.png')
+fig = rc.visualize(df=weighted_triangle_df)
+fig.savefig(_REPORTS_FIGURES_PATH + 'retention.png')
+fig.close()
 
-# Fit time series to last 180 days of DNU numbers
+# Evaluate last _DAYS days of DNU numbers as time series
+dnu_time_series_df = df.tail(_DAYS)
+dnu_time_series_df = dnu_time_series_df.reset_index(drop=True)
+
+cols = ['DATE', 'DNU']
+dnu_time_series_df = dnu_time_series_df[cols]
+dnu_time_series_df['DATE'] = pd.to_datetime(dnu_time_series_df['DATE'])
+
+# Visualize DNU time series
+nu = NewUsers()
+fig = nu.visualize(df=dnu_time_series_df)
+fig.savefig(_REPORTS_FIGURES_PATH + 'dnu.png')
+fig.close()
+
+# Fit (simple) new user forecast model
+nu.fit(df=dnu_time_series_df)
